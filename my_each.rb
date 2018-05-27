1ab5ce58032fc9346b9d410861831bbed220a912
Matt Farmer <mrfarmer777@gmail.com>
2018-05-27 02:35:46 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(array)
   # code here
+  if block_given?
+    i=0
+    while i<array.size do
+      yield array[i]
+      i+=1
+    end
+  else
+    'No block was given'
+  end
 end
